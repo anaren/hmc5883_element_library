91eecda6345f608519d4d5c800524fd755d53c60
klesseranaren <klesser@anaren.com>
2016-06-10 09:57:21 -0400

    fix element_code functions to use updated data types

--- element_code/rawMagneticField.c
+++ element_code/rawMagneticField.c
@@ -1,18 +1,18 @@
 	
 	HMC5883_SetOperatingMode(HMC5883_SINGLEMEASUREMENT);
-	uint8 xmsb = HMC5883_ReadReg(0x03);
-	uint8 xlsb = HMC5883_ReadReg(0x04);
-	int16 HMC5883x = ((int16)xmsb << 8) | xlsb;
+	uint8_t xmsb = HMC5883_ReadReg(0x03);
+	uint8_t xlsb = HMC5883_ReadReg(0x04);
+	int16_t HMC5883x = ((int16_t)xmsb << 8) | xlsb;
 
-	uint8 zmsb = HMC5883_ReadReg(0x05);
-	uint8 zlsb = HMC5883_ReadReg(0x06);
-	int16 HMC5883z = ((int16)zmsb << 8) | zlsb;
+	uint8_t zmsb = HMC5883_ReadReg(0x05);
+	uint8_t zlsb = HMC5883_ReadReg(0x06);
+	int16_t HMC5883z = ((int16_t)zmsb << 8) | zlsb;
 
-	uint8 ymsb = HMC5883_ReadReg(0x07);
-	uint8 ylsb = HMC5883_ReadReg(0x08);
-	int16 HMC5883y = ((int16)ymsb << 8) | ylsb;
+	uint8_t ymsb = HMC5883_ReadReg(0x07);
+	uint8_t ylsb = HMC5883_ReadReg(0x08);
+	int16_t HMC5883y = ((int16_t)ymsb << 8) | ylsb;
 	
-	char valueBuffer[32];
+	static char valueBuffer[32];
 	
 	sprintf(valueBuffer, "[%d,%d,%d]", HMC5883x, HMC5883y, HMC5883z);
 	
